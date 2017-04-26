588893ae5768bbfd964c730acc9dce97b7da6773
RonHsiao <blurryeyes14@gmail.com>
2017-04-26 21:26:35 +0800

    爬蟲

--- cnyestest.py
+++ cnyestest.py
@@ -10,6 +10,6 @@ url = "http://news.cnyes.com/api/v2/news"
 pl = {'startAt': '1492272000', 'endAt': '1493222399', 'limit': '30'}
 headers = {"User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
 res = requests.get(url, params=pl).json()
-
-# test = json(res)
-print(res)
+res1 = json.dumps(res)
+rr = json.loads(res1)
+print(rr['items']['data'][0]['newsId'])
