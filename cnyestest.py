9ec28b03a0aff3271afc6af03381a31fb8d28a02
RonHsiao <blurryeyes14@gmail.com>
2017-04-28 13:33:53 +0800

    爬蟲

--- cnyestest.py
+++ cnyestest.py
@@ -1,15 +1,76 @@
 import requests
 import time
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from requests import Request, Session
 import json
 
-url = "http://news.cnyes.com/api/v2/news"
+url = "http://news.cnyes.com/api/v3/news/category/tw_stock"
+Sat = 1493049600
+Eat = 1493135999
+# Sat = 1356969600  # 20130101 00點00分00秒
+# Eat = 1357055999  # 20130101 23點59分59秒
+page = 1
+pl = {'startAt': Sat, 'endAt': Eat, 'limit': '30', 'page': page}
+lastpage = 1
+f = open('D:\TestStock.txt', 'a', encoding='UTF-8')
+while Sat < 1493222400:
+    page = 1
+    print("start")
+    while (lastpage - page) != -1:
+        print("check page")
+        res = requests.get(url, params=pl)
+        time.sleep(3)
+        res.close()
+        restoJson = res.json()
+        resJson = json.dumps(restoJson)
+        rdj = json.loads(resJson)
+        page += 1
+        lastpage = int(rdj['items']['last_page'])
+        dataid = 0
+        iftotal = rdj['items']['total']
+        iftotal30 = iftotal % 30
+        if (lastpage - page) == 0 or int(iftotal) < 30:
+            print("data<30 or lastpage")
+            while dataid < iftotal30:
+                newsid = rdj['items']['data'][dataid]['newsId']
+                newsurl = "http://news.cnyes.com/news/id/" + str(newsid)
+                newsres = requests.get(newsurl, headers={"Accept": "image/webp,image/*,*/*;q=0.8",
+                                                         "Accept-Encoding": "gzip, deflate, sdch",
+                                                         "Accept-Language": "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4",
+                                                         "Connection": "keep-alive",
+                                                         "Referer": "http://news.cnyes.comnews/id/3792704",
+                                                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"})
+                newsres.encoding = 'utf'
+                soup = BeautifulSoup(newsres.text, "lxml")
+                newsres.close()
+                time.sleep(3)
+                for soup2 in soup.select(
+                        '._82F p'):  # 用for迴圈取 會按照網頁<p>順序依序取出
+                    f.write(soup2.get_text(separator="\n\n", strip=True))
+                f.write("\n\n")
+                dataid += 1
+        else:
+            while dataid < 30:
+                print("loading"+str(dataid))
+                newsid = rdj['items']['data'][dataid]['newsId']
+                newsurl = "http://news.cnyes.com/news/id/" + str(newsid)
+                newsres = requests.get(newsurl, headers={"Accept": "image/webp,image/*,*/*;q=0.8",
+                                                         "Accept-Encoding": "gzip, deflate, sdch",
+                                                         "Accept-Language": "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4",
+                                                         "Connection": "keep-alive",
+                                                         "Referer": "http://news.cnyes.comnews/id/3792704",
+                                                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"})
+                newsres.encoding = 'utf'
+                soup = BeautifulSoup(newsres.text, "lxml")
+                newsres.close()
+                time.sleep(3)
+                for soup2 in soup.select(
+                        '._82F p'):  # 用for迴圈取 會按照網頁<p>順序依序取出
+                    f.write(soup2.get_text(separator="\n\n", strip=True))
+                f.write("\n\n")
+                dataid += 1
+    print("Sat="+str(Sat))
+    print("page=" + str(page))
+    Sat = Sat + 86400
+    Eat = Eat + 86400
 
-pl = {'startAt': '1492272000', 'endAt': '1493222399', 'limit': '30'}
-headers = {"User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
-res = requests.get(url, params=pl).json()
-res1 = json.dumps(res)
-rr = json.loads(res1)
-print(rr['items']['data'][0]['newsId'])
+f.close()
